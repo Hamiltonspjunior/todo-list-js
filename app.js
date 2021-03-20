b2eb0b298eb59e9d5e3cef029cb414f8254a1791
Hamiltonspjunior <hamiltonspjunior@gmail.com>
2021-03-20 12:17:16 -0300

    Refactoring

--- app.js
+++ app.js
@@ -2,64 +2,67 @@ const formAddTodo = document.querySelector('.form-add-todo')
 const inputSearch = document.querySelector('.form-search input')
 const todosContainer = document.querySelector('.todos-container')
 
-const addNewTodo = value => {
-  todosContainer.innerHTML += `
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-      <span>${value}</span>
-      <i class="far fa-trash-alt delete"></i>
-    </li>
-  `
+const addTodo = inputValue => {
+  if (inputValue.length) {
+    todosContainer.innerHTML += `
+      <li class="list-group-item d-flex justify-content-between align-items-center" data-todo="${inputValue}">
+        <span>${inputValue}</span>
+        <i class="far fa-trash-alt" data-trash="${inputValue}"></i>
+      </li>
+    `
+    formAddTodo.reset()
+  }
 }
 
-const getTodosThatNotMatchWithSearch = (todos, inputSearchValue) => todos
-  .filter(todo => !todo.textContent.toLowerCase().includes(inputSearchValue))
+const removeTodo = clickedElement => {
+  const trashDataValue = clickedElement.dataset.trash
+  const todo = document.querySelector(`[data-todo="${trashDataValue}"]`)
+  
+  if (trashDataValue) {
+    todo.remove()
+  }
+}
 
-const getTodosThatMatchWithSearch = (todos, inputSearchValue) => todos
-  .filter(todo => todo.textContent.toLowerCase().includes(inputSearchValue))
-
-const hideTodos = todos => {
-  todos.forEach(todo => {
-    todo.classList.remove('d-flex')
-    todo.classList.add('hidden')
+const filterTodos = (todos, inputValue, returnMatchedTodos) => {
+  return todos.filter(todo => {
+    const matchedTodos = todo.textContent.toLowerCase().includes(inputValue)
+    return returnMatchedTodos ? matchedTodos : !matchedTodos
   })
 }
 
-const showTodos = todos => {
+const manipulateClasses = (todos, classToAdd, classToRemove) => {
   todos.forEach(todo => {
-    todo.classList.remove('hidden')
-    todo.classList.add('d-flex')
+    todo.classList.remove(classToRemove)
+    todo.classList.add(classToAdd)
   })
+}
+
+const hideTodos = (todos, inputValue) => {
+  const todosToHide = filterTodos(todos, inputValue, false)
+  manipulateClasses(todosToHide, 'hidden', 'd-flex')
+}
+
+const showTodos = (todos, inputValue) => {
+  const todosToShow = filterTodos(todos, inputValue, true)
+  manipulateClasses(todosToShow, 'd-flex', 'hidden')
 }
 
 formAddTodo.addEventListener('submit', event => {
   event.preventDefault()
 
   const inputValue = event.target.add.value.trim()
-  const isInputNotEmpty = inputValue.length 
-
-  if (isInputNotEmpty) {
-    addNewTodo(inputValue)
-    formAddTodo.reset()
-  }
+  addTodo(inputValue)
 })
 
 todosContainer.addEventListener('click', event => {
   const clickedElement = event.target
-  const clickedElementClasses = Array.from(clickedElement.classList)
-  const shouldElementDeleteTodo = clickedElementClasses.includes('delete')
-
-  if (shouldElementDeleteTodo) {
-    clickedElement.parentElement.remove()
-  }
+  removeTodo(clickedElement)
 })
 
 inputSearch.addEventListener('input', event => {
   const inputValue = event.target.value.trim().toLowerCase()
   const todos = Array.from(todosContainer.children)
-
-  const todosThatNotMatchWithSearch = getTodosThatNotMatchWithSearch(todos, inputValue)
-  const todosThatMatchWithSearch = getTodosThatMatchWithSearch(todos, inputValue)
   
-  hideTodos(todosThatNotMatchWithSearch)
-  showTodos(todosThatMatchWithSearch)
+  hideTodos(todos, inputValue)
+  showTodos(todos, inputValue)
 })
